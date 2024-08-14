157ba46caebbd381a283c53cbcae9d1482818c44
Filipe Liu <fliu@anl.gov>
2024-08-14 13:42:57 -0500

    ups

--- lib/kb_deeptransyt/kb_deeptransytImpl.py
+++ lib/kb_deeptransyt/kb_deeptransytImpl.py
@@ -1,31 +1,82 @@
+# -*- coding: utf-8 -*-
 #BEGIN_HEADER
 import logging
 import os
 
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
-#BEGIN_CLASS_HEADER
-#END_CLASS_HEADER
-#BEGIN_CONSTRUCTOR
+
+
+class kb_deeptransyt:
+    '''
+    Module Name:
+    kb_deeptransyt
+
+    Module Description:
+    A KBase module: kb_deeptransyt
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = "git@github.com:Fxe/kb_deeptransyt.git"
+    GIT_COMMIT_HASH = "5855499b63c264782e53366e8822e14f624d0992"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
-#END_CONSTRUCTOR
-#BEGIN run_kb_deeptransyt
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_kb_deeptransyt(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_kb_deeptransyt
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report': {'objects_created':[],
                                                 'text_message': params['parameter_1']},
                                                 'workspace_name': params['workspace_name']})
+
+        # deep_transyt.run(xxxxx)
+        print('works!')
+
         output = {
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
         }
-#END run_kb_deeptransyt
-#BEGIN_STATUS
+        #END run_kb_deeptransyt
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_kb_deeptransyt return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-#END_STATUS
+        #END_STATUS
+        return [returnVal]
