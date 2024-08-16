9302c830e98407b2899e54d84a938c0bea79d894
Apolinario8 <goncalocardoso2016@gmail.com>
2024-08-16 12:07:55 -0500

    fixing output

--- lib/kb_deeptransyt/kb_deeptransytImpl.py
+++ lib/kb_deeptransyt/kb_deeptransytImpl.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 #BEGIN_HEADER
-import logging
 import os
+import shutil
+import uuid
 
 from predict_genome import *
 
+from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.KBaseReportClient import KBaseReport
+from installed_clients.WorkspaceClient import Workspace as workspaceService
 #END_HEADER
 
 
@@ -33,29 +36,57 @@ class kb_deeptransyt:
 
     # config contains contents of config file in a hash or None if it couldn't
     # be found
-    def __init__(self, config):
-        #BEGIN_CONSTRUCTOR
-        self.callback_url = os.environ['SDK_CALLBACK_URL']
-        self.shared_folder = config['scratch']
-        logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
-                            level=logging.INFO)
-        #END_CONSTRUCTOR
-        pass
+    def _get_input_file_ref_from_params(self, params):
+        if 'input_genome' in params:
+            return params['input_genome']
+        else:
+            if 'input_ws' not in params and 'input_file' not in params:
+                raise ValueError('Either the "input_genome" field or the "input_ws" with "input_file" fields must be set.')
+            return str(params['input_ws']) + '/' + str(params['input_file'])
 
+    def create_report(self, token, ws, uuid_string, output_dir):
+        output_files = [{'shock_id': self.dfu.file_to_shock({
+                                'file_path': os.path.join(output_dir, 'final_predictions.csv'),
+                                'make_handle': 0,
+                                'pack': 'zip'})['shock_id'],
+                        'name': 'final_predictions.csv',
+                        'label': 'Predicted Results',
+                        'description': 'CSV file containing the predicted results'}]
+
+        report_params = {
+            'file_links': output_files,
+            'workspace_name': ws,
+            'report_object_name': 'kb_deeptransyt_report_' + uuid_string
+        }
+
+        kbase_report_client = KBaseReport(self.callback_url, token=token)
+        output = kbase_report_client.create_extended_report(report_params)
+        return output
+
+    def __init__(self, config):
+        self.workspaceURL = config['workspace-url']
+        self.scratch = os.path.abspath(config['scratch'])
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.dfu = DataFileUtil(self.callback_url)
+        pass
 
     def run_kb_deeptransyt(self, ctx, params):
         """
-        This example function accepts any number of parameters and returns results in a KBaseReport
+        This function runs the deeptransyt model, processes input sequences, and returns a KBaseReport.
         :param params: instance of mapping from String to unspecified object
         :returns: instance of type "ReportResults" -> structure: parameter
            "report_name" of String, parameter "report_ref" of String
         """
 
+        token = ctx['token']
+        uuid_string = str(uuid.uuid4())
+        output_dir = os.path.join(self.scratch, uuid_string)
+        os.mkdir(output_dir)
+
         input_dir = params['input_dir']
-        output_dir = params['output_dir']
         preprocess = params.get('preprocess', True)
 
-        # Running the deeptransyt process
+        # Preprocessing
         df_sequences = load_sequences(input_dir)
         encodings, labels = create_encodings(df_sequences, input_dir, preprocess=preprocess)
         
@@ -71,7 +102,7 @@ class kb_deeptransyt:
         df_subfamily_predictions = predict_subfamily(positive_encodings, positive_labels)
         df_metabolic_predictions = predict_metabolic_important(positive_encodings, positive_labels)
         
-        # Merging 
+        # Merging results
         df_merged = df_binary_predictions.merge(df_family_predictions, on='Accession', how='left')
         df_merged = df_merged.merge(df_subfamily_predictions, on='Accession', how='left')
         df_merged = df_merged.merge(df_metabolic_predictions, on='Accession', how='left')
@@ -79,29 +110,11 @@ class kb_deeptransyt:
         # Saving final csv
         final_output_path = os.path.join(output_dir, 'final_predictions.csv')
         df_merged.to_csv(final_output_path, index=False)
-        
-        # ctx is the context object
-        # return variables are: output
-        #BEGIN run_kb_deeptransyt
-        report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': params['parameter_1']},
-                                                'workspace_name': params['workspace_name']})
 
-        # deep_transyt.run(xxxxx)
-        print('works!', params)
+        output = self.create_report(token, params['workspace_name'], uuid_string, output_dir)
 
-        output = {
-            'report_name': report_info['name'],
-            'report_ref': report_info['ref'],
-        }
-        #END run_kb_deeptransyt
+        shutil.rmtree(output_dir, ignore_errors=True)
 
-        # At some point might do deeper type checking...
-        if not isinstance(output, dict):
-            raise ValueError('Method run_kb_deeptransyt return value ' +
-                             'output is not type dict as required.')
-        # return the results
         return [output]
     
     def status(self, ctx):
